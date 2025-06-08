a7e3d4305d9de3fca7c7f5dc5581d02c8eaa88cc
zeks07 <76873691+zeks07@users.noreply.github.com>
2025-06-08 10:44:06 +0200

    Revert "Boss template"

--- igrica/scripts/hitbox.gd
+++
@@ -1,9 +0,0 @@
-class_name Hitbox
-
-extends Area2D
-
-func _process(_delta):
-	var targets = get_overlapping_bodies()
-	for target in targets:
-		if target.has_method("take_damage"):
-			target.take_damage()
